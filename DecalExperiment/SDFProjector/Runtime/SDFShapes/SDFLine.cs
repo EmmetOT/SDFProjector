329ce01c6992607bcbd2c7b189c4faab2aed10a0
emmy <emmetot@klang-games.com>
2024-12-02 19:12:45 +0100

    unused import

--- DecalExperiment/SDFProjector/Runtime/SDFShapes/SDFLine.cs
+++ DecalExperiment/SDFProjector/Runtime/SDFShapes/SDFLine.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Seed.DecalProjector {
